4935a456733d7fc5248840395adae1a0f0e7bba1
Abderrahmen Dai <abderrahmen.dai.11@gmail.com>
2022-09-18 21:04:12 +0100

    stage2

--- src/app/room/home/stage2/stage2.component.ts
+++ src/app/room/home/stage2/stage2.component.ts
@@ -19,53 +19,59 @@ interface DandDList {
 @Component({
   selector: 'app-stage2',
   templateUrl: './stage2.component.html',
-  styleUrls: ['./stage2.component.scss']
+  styleUrls: ['./stage2.component.scss'],
 })
-
-export class Stage2Component   {
-
+export class Stage2Component {
   
+  todo: DandDList = {
+    bigName: 'todo',
+    smallName: 'this text is small',
+    color: 'purpule',
+    labels: [
+      { value: 'handle erreurs', color: 'red' },
+      { value: 'use console.log', color: 'blue' },
+      { value: 'fix erreurs', color: 'green' },
+      { value: 'fix erreurs', color: 'green' },
+      { value: 'fix erreurs', color: 'green' },
+    ],
+  };
+  inPrograss: DandDList = {
+    bigName: 'doing',
+    smallName: 'this text is small',
+    color: 'black',
+    labels: [
+      { value: 'handle erreurs', color: 'red' },
+      { value: 'use console.log', color: 'blue' },
+      { value: 'fix erreurs', color: 'green' },
+      { value: 'fix erreurs', color: 'green' },
+    ],
+  };
 
-  title = 'ab';
-  createdListes: DandDlabel[] = [
-    { value: 'handle erreurs', color: 'red' },
-    { value: 'use console.log', color: 'blue' },
-    { value: 'fix erreurs', color: 'green' },
-    { value: 'fix erreurs', color: 'green' },
-  ];
-  DandDLists: DandDList[] = [
-    {
-      bigName: 'todo',
-      smallName: 'this text is small',
-      color: 'purpule',
-      labels: [
-        { value: 'handle erreurs', color: 'red' },
-        { value: 'use console.log', color: 'blue' },
-        { value: 'fix erreurs', color: 'green' },
-        { value: 'fix erreurs', color: 'green' },
-        { value: 'fix erreurs', color: 'green' },
-      ],
-    },
-    {
-      bigName: 'doing',
-      smallName: 'this text is small',
-      color: 'black',
-      labels: [
-        { value: 'handle erreurs', color: 'red' },
-        { value: 'use console.log', color: 'blue' },
-        { value: 'fix erreurs', color: 'green' },
-        { value: 'fix erreurs', color: 'green' },
-      ],
-    },
-    {
-      bigName: 'done',
-      smallName: 'this text is small',
-      color: 'blue',
-      labels: [
-        { value: 'handle erreurs', color: 'red' },
-        { value: 'use console.log', color: 'blue' },
-        { value: 'fix erreurs', color: 'green' },
-      ],
-    },
-  ];
+  done: DandDList = {
+    bigName: 'done',
+    smallName: 'this text is small',
+    color: 'blue',
+    labels: [
+      { value: 'handle erreurs', color: 'red' },
+      { value: 'use console.log', color: 'blue' },
+      { value: 'fix erreurs', color: 'green' },
+    ],
+  };
+
+  drop(event: CdkDragDrop<DandDlabel[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
